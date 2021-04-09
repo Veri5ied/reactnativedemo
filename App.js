f68731df72e46eb88cf3577a810943d07dc79505
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-04-09 12:06:12 +0100

    box sample

--- App.js
+++ App.js
@@ -1,111 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, View, SafeAreaView, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Here are some boxes of different colors</Text>
+      <View>
+        <Text style={[styles.cyanColor, styles.spacing, styles.text]}>
+          Cyan
+        </Text>
+      </View>
+      <View>
+        <Text style={[styles.blueColor, styles.spacing, styles.text]}>
+          Blue
+        </Text>
+      </View>
+      <View>
+        <Text style={[styles.magentoColor, styles.spacing, styles.text]}>
+          Magento
+        </Text>
+      </View>
+      <View>
+        <Text style={[styles.orangeColor, styles.spacing, styles.text]}>
+          Orange
+        </Text>
+      </View>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    padding: 10,
+    margin: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  cyanColor: {
+    backgroundColor: 'cyan',
   },
-  highlight: {
-    fontWeight: '700',
+
+  blueColor: {
+    backgroundColor: 'blue',
+  },
+
+  magentoColor: {
+    backgroundColor: '#d33682',
+  },
+
+  orangeColor: {
+    backgroundColor: '#cb4b16',
+  },
+
+  spacing: {
+    margin: 4,
+    padding: 10,
+    color: '#ffffff',
+  },
+
+  viewBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
